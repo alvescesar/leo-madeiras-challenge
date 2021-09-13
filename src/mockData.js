ecc18950a25b8b619b560640da161d581625552d
César Alves <alvescesar@hotmail.com>
2021-09-13 12:29:38 -0300

    Layout tests done

---
+++ src/mockData.js
@@ -0,0 +1,34 @@
+export const noUser = {
+  nome: '',
+  cpf: '',
+  email: '',
+  telefone: '',
+};
+
+export const editUser = {
+  nome: 'Leodolmira Guazelli',
+  cpf: '55566644455',
+  email: 'mirinha@bol.com.br',
+  telefone: '11988665533',
+};
+
+export const userList = [
+  {
+    nome: 'Aderbaldo Martins',
+    cpf: '12345678900',
+    email: 'aderbaldo@bol.com.br',
+    telefone: '5432313232',
+  },
+  {
+    nome: 'Etelvina Pinheiros',
+    cpf: '09876543211',
+    email: 'etelvina@bol.com.br',
+    telefone: '55988776655',
+  },
+  {
+    nome: 'Teotonio Luiz',
+    cpf: '90990990990',
+    email: 'totonho@bol.com.br',
+    telefone: '1665432100',
+  },
+];
